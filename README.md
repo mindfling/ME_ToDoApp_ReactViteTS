d38e84cb3c6f013d63590a1772ce01f5ca8ee488
Nepochtov Dmitry <dimendroider@gmail.com>
2023-10-17 09:48:14 +0300

    remove files

--- README.md
+++ README.md
@@ -5,13 +5,6 @@
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## Expanding the ESLint configuration
-
 
 ## Working commands
 
@@ -27,3 +20,5 @@ on gitBash
 
 `npm i`
 `npm run dev`
+
+`npm r eslint`
--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [discount, setDiscount] = useState(0);
+
+  const my = 3; // eslint, must die, say error here as never used
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button onClick={() => setDiscount((discount) => discount - 1)}>discount minus {discount}</button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edited summ is {count + discount}
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      
     </>
   )
 }
