d1bcc52b142879e75526f2f19dfadce74132c038
Nepochtov Dmitry <dimendroider@gmail.com>
2023-10-18 10:42:58 +0300

    feat app

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,34 @@
-import { Alert, Breadcrumb, Button, CloseButton, Table } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import { Title } from './Components/Title';
 import ExampleModal from './Components/ExampleModal';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form } from './Components/Form';
 import MyTable from './Components/MyTable';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
 
   return (
-    <div className='App app-container'>
+    <div className='App app-container  d-flex align-items-center justify-content-center flex-column'>
       <ExampleModal /> {/* modal shows at start */}
 
       <Title title='ToDoWka Application' />
 
-      <Button variant="primary">Primary</Button>{' '}
-      <Button variant="secondary">Secondary</Button>{' '}
-      <Button variant="success">Success</Button>{' '}
-      <Button variant="warning">Warning</Button>{' '}
-      <Button variant="danger">Danger</Button>{' '}
-      <Button variant="info">Info</Button>{' '}
-      <Button variant="light">Light</Button>{' '}
-      <Button variant="dark">Dark</Button>
-      <Button variant="link" className='mb-5'>Link</Button>
+      <Container>
+        <Button variant="primary">Primary</Button>{' '}
+        <Button variant="secondary">Secondary</Button>{' '}
+        <Button variant="success">Success</Button>{' '}
+        <Button variant="warning">Warning</Button>{' '}
+        <Button variant="danger">Danger</Button>{' '}
+        <Button variant="info">Info</Button>{' '}
+        <Button variant="light">Light</Button>{' '}
+        <Button variant="dark">Dark</Button>
+        <Button variant="link" className='mb-5'>Link</Button>
+      </Container>
 
-      <Alert>Add some task ToDo</Alert>
-      <Form /> {/* form add task */}
+      <Form />
 
-      <MyTable></MyTable>
-
-{/*
-      <MyTable variant='dark' hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>oeau</td>
-            <td>Larry the Bird</td>
-            <td>
-              <Button variant='warning' className='mx-1'>twitter</Button>
-              <Button variant='light'>twitter</Button>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-*/}
+      <MyTable />
     </div>
   )
 }
