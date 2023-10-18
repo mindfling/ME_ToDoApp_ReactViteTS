fafa4086d3f8390f967ba5bb699491494fb91fe1
Nepochtov Dmitry <dimendroider@gmail.com>
2023-10-18 23:02:48 +0300

    feat without test Button colors in React

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-
   return (
     <div className='App app-container  d-flex align-items-center justify-content-center flex-column'>
       <ExampleModal /> {/* modal shows at start */}
 
       <Title text='ToDoWka Application' />
-{/* 
-      <p>bootstrap buttons test</p>
-      <Container>
-        <Button variant="primary">Primary</Button>{' '}
-        <Button variant="secondary">Secondary</Button>{' '}
-        <Button variant="success">Success</Button>{' '}
-        <Button variant="warning">Warning</Button>{' '}
-        <Button variant="danger">Danger</Button>{' '}
-        <Button variant="info">Info</Button>{' '}
-        <Button variant="light">Light</Button>{' '}
-        <Button variant="dark">Dark</Button>
-        <Button variant="link" className='mb-5'>Link</Button>
-      </Container>
-*/}
 
       <Form />
 
