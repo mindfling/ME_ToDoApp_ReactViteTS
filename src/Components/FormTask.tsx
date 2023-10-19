cac748be48cdef8ba1a67a5c5e7d4b1f77785fdc
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-10-19 20:04:41 +0300

    feat form works with store

--- src/Components/FormTask.tsx
+++ src/Components/FormTask.tsx
@@ -2,34 +2,51 @@
 import Form from 'react-bootstrap/Form';
 import { Button, FormControl } from "react-bootstrap";
 import React, { useState } from 'react';
+import { Store } from '../modules/Store';
+import { ITask, Task } from '../modules/Task';
 
-// type Props = {}
 
-export const FormTask = () => {
+interface Props {
+  store: Store;
+}
+
+export const FormTask = ({store}: Props) => {
+  console.log('store in FormTask: ', store);
   
-  const [value, setValue] = useState('');
-
+  const [value, setValue] = useState(''); // input value
+  
+  // * submit
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log('submited and save to storage');
-    console.log(e.target);
-    console.log(e.currentTarget.name);
-    console.log('after submit value: \"', value.replace(/(\s\s*)/g, ' ').toLowerCase(), '\"'); // удаляем лишние пробелы
+    const description: string = value.replace(/(\s\s*)/g, ' ').toLowerCase().trim();
+    // console.log('after submit value: \"', value.replace(/(\s\s*)/g, ' ').toLowerCase(), '\"'); // удаляем лишние пробелы
+    console.log('description: ', description);
+    
+    const task: ITask = new Task(description, 'light');
+    console.log('task in FormTask: ', task);
+    
+    store.addTask(task);
+    console.log('add new task to store: ', store);
   }
   
+  // * simple button click
   const handleButtonClick = (e: React.FormEvent<HTMLButtonElement>) => {
     console.log('click button submit', e.currentTarget);
   }
   
+  // * reset
   const handleClickReset = (e: React.FormEvent<HTMLButtonElement>) => {
     console.log('click button reset', e.currentTarget);
   }
+  
 
   return (
     <Form className='form-task d-flex justify-content my-3' onSubmit={handleSubmit}>
       <FormControl
         type='text'
         placeholder='Начните вводить...'
+        title='Просто введите краткое описание задачи'
         name='task'
         value={value}
         onChange={(e) => setValue(e.target.value)}
