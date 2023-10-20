f2fc04427ad069e51be1e365f4fb78c861143f6e
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-10-20 18:18:41 +0300

    feat app

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,34 @@
-// import { Button, Container } from 'react-bootstrap';
-import { Title } from './Components/Title';
-import ExampleModal from './Components/ExampleModal';
-import { FormTask } from './Components/FormTask';
-import MyTable from './Components/MyTable';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Title } from './Components/Title';
+import { FormTask } from './Components/FormTask';
+import { MyTable } from './Components/MyTable';
 import { TITLE, STORAGE_KEY } from './Data/consts';
 import { Store } from './modules/Store';
 import { useState } from 'react';
+import { useToggle } from './modules/hooks';
+import { Button } from 'react-bootstrap';
+import { ModalForm } from './Components/ModalForm';
+
 console.log('TITLE: ', TITLE);
 console.log('STORAGE_KEY: ', STORAGE_KEY);
 
 
 function App() {
-  
-  // const store: Store = new Store(STORAGE_KEY);
-  
   const [store, setStore] = useState(new Store(STORAGE_KEY));
+  const [isVisible, setVisible] = useToggle(false);
 
   return (
-    <div className='App app-container  d-flex align-items-center justify-content-center flex-column'>
-      <ExampleModal />
+    <div className='app app-container  d-flex align-items-center justify-content-center flex-column'>
+      <ModalForm isVisible={isVisible} />
+
       <Title text={TITLE} />
       <FormTask store={store} />
+
+      <Button onClick={(e) => {
+        console.log(e.target);
+        setVisible((isVisible: boolean) => !isVisible);
+      }} type='button' className='my-3'>toggle modal</Button>
+
       <MyTable store={store} />
     </div>
   )
