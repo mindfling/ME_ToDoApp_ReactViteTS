fc7827a651e803da0339ec915f95dac998807405
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-10-21 20:10:38 +0300

    Update consts.ts

--- src/Data/consts.ts
+++ src/Data/consts.ts
@@ -1,6 +1,5 @@
 // * константы настройки
-export const TITLE: string = 'Приложение Список дел';
-export const KEY: string = 'todo_';
+export const TITLE: string = 'Список дел на React + Vite';
+export const KEY: string = 'todo';
 
 export const STORAGE_KEY: string = KEY;
-
