7e56176bd1bfe093025ff6528de13e23acaca642
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-10-21 19:52:36 +0300

    feat useLocalStorage

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import { Title } from './Components/Title';
 import { FormTask } from './Components/FormTask';
 import { TableTask } from './Components/TableTask';
 import { TITLE, STORAGE_KEY } from './Data/consts';
-import { useState } from 'react';
 import { useToggle } from './modules/Hooks/useToggle';
 import { ModalForm } from './Components/ModalForm';
 import { ITask } from './modules/Task';
+import { useLocalStorage } from './modules/Hooks/useLocalStorage'; // мой hook
 
 console.log('TITLE: ', TITLE);
 console.log('STORAGE_KEY: ', STORAGE_KEY);
@@ -16,43 +16,34 @@ export const App = () => {
 
   const [isVisible, setVisible] = useToggle(true);
 
-  const rawData: string | null = localStorage.getItem(STORAGE_KEY);
-  const testData: Array<ITask> = (rawData) ? JSON.parse(rawData) : [];
-  const [data, setData] = useState(testData);
-
+  const [data, setData] = useLocalStorage([], STORAGE_KEY);
 
   const handleRemoveClick = (event: React.MouseEvent<HTMLButtonElement>, id: string) => {
-    console.log('Remove click', id);
-    // remove from array
     setData((data) => {
       const newArray: Array<ITask> = data.filter((task) => task.id !== id);
-      localStorage.setItem(STORAGE_KEY, JSON.stringify(newArray)); // сохраняем в хранилище
       return newArray;
     });
   }
 
   const handleDoneClick = (event: React.MouseEvent<HTMLButtonElement>, id: string) => {
-    console.log('Done click', id);
-    // make task done and update array
     setData((data) => {
       const newArray = data.map((task) => {
         const newTask: ITask = (task.id === id) ? { ...task, status: 'done' } : { ...task };
         return newTask;
       });
-      localStorage.setItem(STORAGE_KEY, JSON.stringify(newArray)); // сохраняем в хранилище
       return newArray;
     });
   }
 
-  
+
 
   const modalClose = () => {
     setVisible(false);
   }
-  
+
   return (
     <div className='app app-container d-flex align-items-center justify-content-center flex-column'>
-      { (isVisible) ? (
+      {(isVisible) ? (
         <>
           <ModalForm isVisible={isVisible} onHide={modalClose} />
           <p>Обновите страницу</p>
@@ -63,7 +54,7 @@ export const App = () => {
           <FormTask data={data} setData={setData} />
           <TableTask data={data} onRemove={handleRemoveClick} onDone={handleDoneClick} />
         </>
-        )
+      )
       }
     </div>
   )
--- src/modules/Hooks/useLocalStorage.ts
+++ src/modules/Hooks/useLocalStorage.ts
@@ -1,2 +1,23 @@
 // todo useLocalStorage
+import React, { useState, useEffect } from 'react';
+import { ITask } from "../Task";
 
+
+// * hook для localStorage
+export const useLocalStorage = (initValue: Array<ITask>, key: string) : [Array<ITask>, React.Dispatch<React.SetStateAction<ITask[]>>] => {
+
+  const getValue = (): Array<ITask> => {
+    const rawData: string | null = localStorage.getItem(key);
+    const testData: Array<ITask> = (rawData) ? JSON.parse(rawData) : initValue;
+    return testData;
+  }
+
+  const [value, setValue] = useState(getValue); //?
+  
+  useEffect(() => {
+    console.log('use effect of', value);
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value]);
+  
+  return [value, setValue];
+}
