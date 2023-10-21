176a20ea1d3a056bf8cec00369797576ea801235
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-10-21 17:40:03 +0300

    Update util.ts

--- src/modules/util.ts
+++ src/modules/util.ts
@@ -13,3 +13,66 @@ export const getRandomInt = (min: number, max: number): number => {
 export const getRandomId: () => string = () =>
   `id_${Math.random().toString(32).substring(2, 10)}_0x${Date.now().toString(16).substring(3, 11)}`;
 // `id-${Math.random().toString(32).substring(2, 10)}-0x-${Date.now().toString(16).substring(3, 11)}`;
+
+
+
+// * transliterate name of user for hash storage key
+// simplified for aur needs
+export const trans = (word: string): string => {
+  const chars: {[key: string]: string} = {
+    'а': 'a',
+    'б': 'b',
+    'в': 'v',
+    'г': 'g',
+    'д': 'd',
+    'е': 'e',
+    'ё': 'e',
+    'э': 'e',
+    'ж': 'zh',
+    'з': 'z',
+    'и': 'i',
+    'ы': 'i',
+    'й': 'j',
+    'к': 'k',
+    'л': 'l',
+    'м': 'm',
+    'н': 'n',
+    'о': 'o',
+    'п': 'p',
+    'р': 'r',
+    'с': 's',
+    'т': 't',
+    'у': 'u',
+    'ф': 'f',
+    'х': 'h',
+    'ц': 'c',
+    'ч': '4',
+    'ш': 'sh',
+    'щ': 'w',
+    'ъ': '',
+    'ь': '',
+    'ю': 'u',
+    'я': 'a',
+    ',': '',
+    '-': '_',
+    '+': '',
+    '*': '',
+    ' ': '_',
+    '\'': '',
+    '"': '',
+    '\\': '',
+    '`': '',
+    '/': '',
+    '': '',
+  };
+  const result = word.toLowerCase().split('').map((char: string) => {
+    return chars[char] || ((chars[char] === '') ? chars[char] : char);
+  }).join('');
+  return result;
+};
+
+
+// login to key
+export const getLoginAuthStorageKey = () => {
+
+};
